8f9ce238a00fc5be84bb3a9fef4a11b34bbedf08
Sandesh <sandeshkoirala1212@gmail.com>
2023-01-11 09:12:49 +0545

    Change Language Setting ma add and implemented

--- app/src/main/java/com/sanjit/sisu2/ui/Setting.java
+++ app/src/main/java/com/sanjit/sisu2/ui/Setting.java
@@ -1,19 +1,23 @@
 package com.sanjit.sisu2.ui;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Switch;
 import android.widget.Toolbar;
 
 import com.sanjit.sisu2.R;
 
-import java.util.Objects;
-
-import io.grpc.Context;
+import java.util.Locale;
 
 public class Setting extends AppCompatActivity {
 
@@ -26,35 +30,91 @@ public class Setting extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        loadLocale();
         setContentView(R.layout.activity_setting);
 
-        getSupportActionBar().hide();
-        switcher= findViewById(R.id.switcher);
+        //change actionbar title,if you  dont change it will be according to your systens default language
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setTitle(getResources().getString(R.string.app_name));
 
+        getSupportActionBar().hide();
+        switcher = findViewById(R.id.switcher);
+        Button changeLang = findViewById(R.id.changelanguage);
+        changeLang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //show AlertDialog to display list of languages
+                showChangeLanguageDialog();
+            }
+        });
         // We used shared preference to save mode if exit the app and go back again
 
         sharedPreferences = getSharedPreferences("MODE", MODE_PRIVATE);
         nightMode = sharedPreferences.getBoolean("night", false); // Light mode is the default mode
 
-        if(nightMode){
+        if (nightMode) {
             switcher.setChecked(true);
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         }
         switcher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(nightMode){
+                if (nightMode) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                    editor= sharedPreferences.edit();
+                    editor = sharedPreferences.edit();
                     editor.putBoolean("night", false);
-            }else{
+                } else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                    editor= sharedPreferences.edit();
+                    editor = sharedPreferences.edit();
                     editor.putBoolean("night", true);
                 }
                 editor.apply();
             }
         });
 
+    }
+
+    private void showChangeLanguageDialog() {
+        final String [] listItems = {"English", "हिंदी"};
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(Setting.this);
+        mBuilder.setTitle("Choose Language...");
+        mBuilder.setSingleChoiceItems(listItems, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (i == 0) {
+                    //English
+                    setLocale("en");
+                    recreate();
+                } else if (i == 1) {
+                    //Hindi
+                    setLocale("hi");
+                    recreate();
+                }
+                //dismiss alert dialog when language selected
+                dialogInterface.dismiss();
+            }
+        });
+        AlertDialog mDialog = mBuilder.create();
+        //show alert dialog
+        mDialog.show();
+
+    }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration configuration = new Configuration();
+        configuration.locale = locale;
+        getBaseContext().getResources().updateConfiguration(configuration, getBaseContext().getResources().getDisplayMetrics());
+        //save data to shared preferences
+        SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        editor.putString("My_Lang", lang);
+        editor.apply();
+    }
+    private void loadLocale() {
+        SharedPreferences prefs = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+        String language = prefs.getString("My_Lang", "");
+        setLocale(language);
     }
 }
