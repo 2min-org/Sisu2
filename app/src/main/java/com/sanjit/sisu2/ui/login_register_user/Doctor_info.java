5a59c449e43acfbd371bf43ff75c5a17a82a7fe0
Sanjit Khanal <sanjitnepalkhanal@gmail.com>
2023-01-15 23:06:56 +0545

    debug3

--- app/src/main/java/com/sanjit/sisu2/ui/login_register_user/Doctor_info.java
+++ app/src/main/java/com/sanjit/sisu2/ui/login_register_user/Doctor_info.java
@@ -3,15 +3,16 @@ package com.sanjit.sisu2.ui.login_register_user;
 import java.util.List;
 
 public class Doctor_info {
-    public String FullName,Email, Specialization,UId;
+    public String FullName,Email, Specialization,UId,Phone,ProfilePic;
     private List <String> appointment_id;
 
-    public Doctor_info(String fullName, String email,String spec,List<String> appointment_id,String UId) {
+
+    public Doctor_info(String fullName, String email,String spec,List<String> appointment_id,String UId,String PhotoUrl) {
         this.FullName=fullName;
         this.Email=email;
         this.Specialization =spec;
         this.appointment_id=appointment_id;
         this.UId=UId;
-
+        this.ProfilePic=PhotoUrl;
     }
 }
